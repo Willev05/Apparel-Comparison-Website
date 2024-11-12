24a9fe31e628117dcb1b1900bffaded95adff215
Shayaan Kashif <shayaankashif27@gmail.com>
2024-11-11 19:37:02 -0500

    Added ability to store shoes in localstoage to add to compare list

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,6 +1,26 @@
 import '../Styles/card.css';
 
 function Card(props){
+
+    const handelAddComparList = ()=>{
+
+        const shoe ={
+            thumbnail: props.thumbnail,
+            title: props.name,
+            price: props.price,
+            brand: props.brand
+
+        }
+
+        const existingShoes = JSON.parse(localStorage.getItem("Compare List")) || [];
+
+        existingShoes.push(shoe);
+        localStorage.setItem("Compare List", JSON.stringify(existingShoes));
+
+
+    }
+
+
     return(
         <div className="card" key={props.key}>
             
@@ -9,7 +29,7 @@ function Card(props){
             <p><strong>Price: </strong>${props.price}</p>
             <p><strong>Brand: </strong>{props.brand}</p>
 
-            <button className='compare'>Add to Compare List</button>
+            <button className='compare' onClick={handelAddComparList}>Add to Compare List</button>
             <button className='wish'>Add to Wishlist</button>
 
         </div>
