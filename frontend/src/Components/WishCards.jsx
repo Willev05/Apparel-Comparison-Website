4afa2917ca290c9495ba19ee0c1250735838a25a
Shayaan Kashif <shayaankashif27@gmail.com>
2024-11-13 19:34:12 -0500

    Minor desgin chnages to the wishcards

--- frontend/src/Components/WishCards.jsx
+++ frontend/src/Components/WishCards.jsx
@@ -6,18 +6,18 @@ function WishCards(props) {
         <div className="card2">
             <h2>{props.title}</h2>
             <img src={props.thumbnail} alt={props.title} width="170" height="150" />
-            <p><strong>Price: </strong>${props.price}</p>
+            <p><strong>Retail Price: </strong>${props.price}</p>
             <p><strong>Brand: </strong>{props.brand}</p>
 
             <p><strong>------------------------------Resellers---------------------------</strong></p>
-            <p><strong>stockX price: </strong>${props.stockXPrice}</p>
-            <p><strong>stockX Link: </strong><a href={props.stockXLink}>{props.stockXLink}</a></p>
+            <p><strong>StockX Price: </strong>${props.stockXPrice}</p>
+            <p><strong>StockX Link: </strong><a href={props.stockXLink}>Purchase from StockX</a></p>
 
-            <p><strong>flightClub price: </strong>${props.flightClubPrice}</p>
-            <p><strong>flightClub Link: </strong><a href={props.flightClubLink}>{props.flightClubLink}</a></p>
+            <p><strong>FlightClub Price: </strong>${props.flightClubPrice}</p>
+            <p><strong>FlightClub Link: </strong><a href={props.flightClubLink}>Purchase from FlightClub</a></p>
 
-            <p><strong>goat price: </strong>${props.goatPrice}</p>
-            <p><strong>goat Link: </strong><a href={props.goatLink}>{props.goatLink}</a></p>
+            <p><strong>Goat Price: </strong>${props.goatPrice}</p>
+            <p><strong>Goat Link: </strong><a href={props.goatLink}>Purchase from Goat</a></p>
         </div>
     );
 }
